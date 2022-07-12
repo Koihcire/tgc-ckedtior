699dcc30979d6d77f7192733142ce66aa076ddd4
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-07-12 21:02:36 +0800

    ckeditor test 2 way binding works

---
+++ tgc-ckeditor/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import CkeditorTest from "./CkeditorTest"
+
+function App() {
+  return (
+    <React.Fragment>
+
+      <CkeditorTest/>
+    </React.Fragment>
+  );
+}
+
+export default App;
