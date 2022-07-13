fe1b039d5f6575dcba6a5850f7566bb0123e27ec
Koihcire <103324968+Koihcire@users.noreply.github.com>
2022-07-13 11:20:43 +0800

    output

--- tgc-ckeditor/src/CkeditorTest.js
+++ tgc-ckeditor/src/CkeditorTest.js
@@ -6,33 +6,40 @@ export default class CkeditorTest extends Component {
     state = {
         data: ""
     }
-    
+
     render() {
         return (
-            <div className="App">
-                <h2>Using CKEditor 5 build in React</h2>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    data={this.state.data}
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                        this.setState({
-                            data: data
-                        })
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
-            </div>
+            <React.Fragment>
+                <div className="App">
+                    <h2>Using CKEditor 5 build in React</h2>
+                    <CKEditor
+                        editor={ClassicEditor}
+                        data={this.state.data}
+                        onReady={editor => {
+                            // You can store the "editor" and use when it is needed.
+                            console.log('Editor is ready to use!', editor);
+                        }}
+                        onChange={(event, editor) => {
+                            const data = editor.getData();
+                            console.log({ event, editor, data });
+                            this.setState({
+                                data: data
+                            })
+                        }}
+                        onBlur={(event, editor) => {
+                            console.log('Blur.', editor);
+                        }}
+                        onFocus={(event, editor) => {
+                            console.log('Focus.', editor);
+                        }}
+                    />
+                </div>
+                <div>
+                    <h1>Output</h1>
+                    {this.state.data}
+                </div>
+            </React.Fragment>
+
         );
     }
 }
